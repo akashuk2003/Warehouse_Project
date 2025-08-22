440d51c1ce29dbee478faab77b17b5fb9f7fd38b
akashuk2003 <akashuk48@gmail.com>
2025-08-22 13:14:00 +0530

    updated requirements

--- warehouse/warehouse/settings.py
+++ warehouse/warehouse/settings.py
@@ -95,14 +95,7 @@ WSGI_APPLICATION = 'warehouse.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST"),
-        "PORT": os.environ.get("DB_PORT"),
-    }
+    'default': env.db(),
 }
 
 
